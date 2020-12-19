ab87448a767e5b43ce55d7e4afefd3a5d0a8b127
= <=>
2020-12-19 08:24:48 +0300

    Initial Commit

---
+++ kriptoCombine/kriptoCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  kriptoCombineApp.swift
+//  kriptoCombine
+//
+//  Created by Batuhan Karababa on 19.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct kriptoCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
